867ebd0d4c81937313b30286f1e183f5f6f26740
Vanialis <vanialis@localhost.localdomain>
2018-05-19 17:57:30 +0200

    is_alpha.c

--- src/is_alpha.c
+++ src/is_alpha.c
@@ -1,9 +1,15 @@
 int	is_alpha(char c)
-{
-	if () {
-
-		return (1);
-	}
-	else
+{	
+	if (c < 48 || (c > 57 && c < 65) || (c > 90 && c < 97) || c > 122)
 		return (0);
+	return(1);
+}
+
+int my_is_alpha(char c)
+{
+	if (		(c >= '0' && c <= '9')
+		|| 	(c >= 'A' && c <= 'Z')
+		|| 	(c >= 'A' && c <= 'Z'))
+		return (1);
+	return (0);
 }
